e5d77af54f1013534fb0d7cb57ceaab8571a0670
Alexey Gerasev <alexey.gerasev@gmail.com>
2020-09-14 21:58:23 +0700

    Add option to convert newlines and ignore trailing whitespaces, update package info and readme

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@
 from setuptools import setup
 
 version = '2.0.1'
-repo_url = 'https://github.com/arjun-menon/test_cmd'
+repo_url = 'https://github.com/agerasev/test_cmd_x'
 download_url = repo_url + '/tarball/' + version
 
-setup(name='test_cmd',
+setup(name='test_cmd_x',
       version=version,
-      description='Tool for black-box testing command-line programs using STDIN, STDOUT and STDERR',
+      description='Tool for black-box testing command-line programs, with extensions',
       long_description=open('README.rst').read(),
       url=repo_url,
       download_url=download_url,
-      author='Arjun G. Menon',
-      author_email='contact@arjungmenon.com',
+      author='Alexey Gerasev',
+      author_email='nthend.iipa@gmail.com',
       keywords='command line terminal functional black box testing arguments stdin stdout stderr',
       license='Apache',
       py_modules=['test_cmd'],
